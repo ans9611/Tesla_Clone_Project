d2bb8206c6062da8f5aab27b04152685eb2a114a
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-03-15 20:32:38 -0400

    added background image to the section componenet

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Section() {
   return (
-    <div>Section</div>
+    <Wrap>Section</Wrap>
   )
 }
 
 export default Section
+
+const Wrap = styled.div`
+	width: 100vh;
+	height: 100vh;
+	background-size: cover;
+  background-position: center;
+  backgorund-repeat: no-repeat;
+	background-image: url('/images/model-3.jpg');
+`
