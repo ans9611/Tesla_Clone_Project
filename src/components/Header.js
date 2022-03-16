9ebfde81ea63c7bb6ca047479bbe22258e588aff
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-03-15 23:55:20 -0400

    added cars menus

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,16 +34,22 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
 `
 
 const Menu = styled.div
 `
   display: flex;
   align-items: center;
+  justify-content: center;
+  flex: 1;
 
   p {
     font-weight: 600;
     text-transform: uppercase;
     padding: 0 10px;
+    flex-wrap: no-wrap;
   }
 `
