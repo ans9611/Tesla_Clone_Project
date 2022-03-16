51c344a281b142d95af25b42f16b97bc54868dff
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-03-16 01:37:24 -0400

    add function to open right menus to the burger button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close'
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
   return (
 		<Container>
 			<a>
@@ -20,10 +22,13 @@ function Header() {
 			<RightMenu>
 				<a href='#'>Shop</a>
 				<a href='#'>TESLA ACCOUNT</a>
-				<CustomMenu />
+				<CustomMenu onClick={() => setBurgerStatus(true)} />
 			</RightMenu>
-			<BurgerNav>
-        <CustomClose />
+
+			<BurgerNav show={burgerStatus}>
+				<CloseWrapper>
+					<CustomClose onClick={() => setBurgerStatus(false)} />
+				</CloseWrapper>
 				<li>
 					<a href='#'>Existing Inventory</a>
 				</li>
@@ -112,9 +117,10 @@ const BurgerNav = styled.div`
   display: flex;
   flex-direction: column;
   text-align: start;
+  transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
   li {
     padding: 15px 0;
-    border-bottomL 1px solid rgba(0, 0, 0, .2);
+    border-bottom: 1px solid rgba(0, 0, 0, .2);
 
     a {
       font-weight: 600;
@@ -124,4 +130,10 @@ const BurgerNav = styled.div`
 `
 
 const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
 `
