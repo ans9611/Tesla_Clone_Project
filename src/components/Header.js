2445e1e41a1375f95a514d8e247f39299f1dbf65
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-03-16 01:39:55 -0400

    finished working on nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -118,6 +118,7 @@ const BurgerNav = styled.div`
   flex-direction: column;
   text-align: start;
   transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+  transition: transform 0.2s;
   li {
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, .2);
