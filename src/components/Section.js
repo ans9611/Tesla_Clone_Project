9053125e79f4b60bfe2398a112be798c8101514b
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-03-15 20:49:27 -0400

    add model S picture & css style

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,8 +3,21 @@ import styled from 'styled-components'
 
 function Section() {
   return (
-    <Wrap>Section</Wrap>
-  )
+		<Wrap>
+			<ItemText>
+				<h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+			</ItemText>
+      <ButtonGroup>
+        <LeftButton>
+          Custom Order
+        </LeftButton>
+        <RightButton>
+          Existing Inventory
+        </RightButton>
+      </ButtonGroup>
+		</Wrap>
+	)
 }
 
 export default Section
@@ -14,6 +27,35 @@ const Wrap = styled.div`
 	height: 100vh;
 	background-size: cover;
   background-position: center;
-  backgorund-repeat: no-repeat;
+  background-repeat: no-repeat;
 	background-image: url('/images/model-3.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; // vertical
+  align-items: center; //horizontal
 `
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom; 30px;
+`
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white:
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`
+
+const RightButton = styled(LeftButton)``
